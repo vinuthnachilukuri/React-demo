76c47411a5e9e5d84fc7ed678f48aa6f42e4f466
Vinuthna Chilukuri <vinuthna.chilukuri24@gmail.com>
2025-09-06 21:23:46 +0530

    Initial commit - react demo project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Temp';
+import Footer from './Footer';
+import Food from './Food';
+function App() {
+  return (
+    <>0
+      <Header />
+      <Food/>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
