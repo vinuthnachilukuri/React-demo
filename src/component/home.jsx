1096b40336f7e329f5367a99732fae4c0ac4882a
Vinuthna Chilukuri <vinuthna.chilukuri24@gmail.com>
2025-09-09 11:00:26 +0530

    Initial commit for vite-projectcls

---
+++ src/component/home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div style={{ backgroundColor: "#97524aff", minHeight: "100vh", padding: "20px" }}>
+      <h1>Welcome to Home Page</h1>
+      <p>This is the home page of our React Router project.</p>
+    </div>
+  );
+}
+export default Home;
---
+++ src/component/payment.jsx
@@ -0,0 +1,9 @@
+function Payment() {
+  return (
+    <div style={{ backgroundColor: "#97524aff", minHeight: "100vh", padding: "20px" }}>
+      <h1>Payment Page</h1>
+      <p>Here you can proceed with payments.</p>
+    </div>
+  );
+}
+export default Payment;
